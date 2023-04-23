9013e3c876a04c0d24c38efccbec84ff5f6aa248
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-04-23 14:53:42 +0200

    [add] loader style + mail print

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,39 @@
-console.log("Hello World!")
-
 const {createApp} = Vue
 
+createApp ({
 
-createApp({
     data(){
         return {
-            HelloMessage : `Hello World!`
+            mailList : [],
+            counter : 10,
+            loading : true
 
         }
+    },
+
+    methods: { 
+
+        getApi(){
+            setTimeout(() => {
+                
+                for(let i = 0; i < this.counter; i++){
+                    
+                    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").
+                    then(result => {
+                        
+                        console.log("result.data.response ->",result.data.response)
+                        
+                        this.mailList.push(result.data.response);
+                        this.loading = false
+                    })
+                }
+            }, 2000);
+            console.log(this.mailList);
+        },
+
+    },
+    mounted(){
+        this.getApi()
     }
+
 }).mount("#app")
